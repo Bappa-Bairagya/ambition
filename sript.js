7cc29717184728a476c6401dbe7d60cb2528063d
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-03-26 10:04:03 +0530

    first commit

---
+++ sript.js
@@ -0,0 +1,7 @@
+var loader = document.querySelector("#loader"); // Selects the element with id "loader"
+var body=document.querySelector("body")
+setTimeout(function() {
+    loader.style.top = "-110%";
+    body.style.overflow="scroll"
+    // Moves the loader up by 100% (presumably out of view)
+}, 3000); // Executes after 2000 milliseconds (2 seconds)
